ade186c745610e9e8675220c1b9fe87ffde046a8
Denis Vinychenko <denisvinychenko@gmail.com>
2024-09-17 11:05:25 +0200

    added custom even

---
+++ C/estremi-pari/estremi-pari/estremi-pari.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+    int e1;
+    int e2;
+    int i;
+
+    printf("primo estremo: ");
+    scanf("%d", &e1);
+    printf("secondo estremo: ");
+    scanf("%d", &e2);
+    i = e1;
+
+    do
+    {
+        printf("%d\n", i);
+        i += 2;
+    } while (i <= e2);
+
+    return 0;
+}

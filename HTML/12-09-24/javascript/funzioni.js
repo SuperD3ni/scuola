89c831302ac67155931bc711e4f1fc1da1e55f0f
Denis Vinychenko <denisvinychenko@gmail.com>
2024-09-17 10:20:07 +0200

    yes

--- HTML/12-09-24/javascript/funzioni.js
+++
@@ -1,9 +0,0 @@
-function somma(){
-    let num1=document.getElementById("i1").value; 
-    let num2=document.getElementById("i2").value; 
-    let sum=num1+num2;
-    document.getElementById("b2").textContent=sum;
-    return 0;
-}
-
-document.getElementById("b1").addEventListener("click", somma);
---
+++ HTML/16-09-24/js/funzioni.js
@@ -0,0 +1,19 @@
+/*let a=10;
+let b=20;
+
+[a, b]=[b, a];
+
+console.log(a, b); //20 10
+*/
+
+let parola="tavolo"
+
+console.log(parola.charAt(0)); //"t"
+
+console.log(parola.length);
+
+for (let car of parola) {
+    
+    console.log(car);
+
+};

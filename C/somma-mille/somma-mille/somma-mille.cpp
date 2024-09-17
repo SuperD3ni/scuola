20b69009a6458b55e6d4e08273e28ed24ba0731a
Denis Vinychenko <denisvinychenko@gmail.com>
2024-09-17 10:35:30 +0200

    sum to 1000

---
+++ C/somma-mille/somma-mille/somma-mille.cpp
@@ -0,0 +1,20 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+int main()
+{
+    float n=0;
+    float r=0;
+
+    do
+    {
+        printf("inserisci numero: ");
+        scanf("%f", &n);
+
+        r = r + n;
+    } while (r < 1000);
+
+    printf("risultato finale: %f", r);
+}
---
+++ C/while/while/while.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int i = 0;
+
+	while (i<1000)
+	{
+		printf("%d\n", ++i);
+	}
+
+    return 0;
+}

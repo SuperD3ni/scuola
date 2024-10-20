f879381ec3a559d6c0114772c18b8032a67cafac
SuperD3ni <denisvinychenko@gmail.com>
2024-10-21 00:33:57 +0200

    added html exercises for 21-10-24 and cleaned up a bit

--- C/amogus(r)/amogus(r)/amogus(r).c
+++ C/amogus(r)/amogus(r)/amogus(r).c
@@ -4,5 +4,18 @@
 
 int main()
 {
-    
+    char parola[4] = { 'C', 'I', 'A', 'O'};
+    char lettera;
+
+    printf("inserisci una lettera: ");
+    scanf("%c", &lettera);
+
+    for (int i = 0; i < 4; i++) {
+        if (parola[i] == lettera) {
+            printf("il piu prima che si vede %c e nel %d posto", lettera, i);
+            i = 5;
+        }
+    }
+
+    return 0;
 }
---
+++ HTML/filter/js/functions.js
@@ -0,0 +1,3 @@
+let parole = ["gatto", "cane", "elefante", "topo", "uccello"];
+let filtered = array.filter(e=>e.length>4);
+console.log(filtered); //gatto elefante uccello

634708ef02d684ba445873bcc799f2f75e5aa11c
SuperD3ni <denisvinychenko@gmail.com>
2025-10-07 20:07:53 +0200

    fixed some bugs and finished hw for 08-10-2025

--- Python/salvadanaioCondiviso/sigma.py
+++ Python/salvadanaioCondiviso/sigma.py
@@ -1,4 +1,4 @@
-class Salvadanaio:
+class Salvadanaio: #classe Salvadanaio
     def __init__(self, soldi=0, ):
         self.soldi = soldi
     def aggiungi(self, somma):
@@ -9,22 +9,25 @@ class Salvadanaio:
     def svuota(self):
         self.soldi = 0
 
-class Fratello:
+class Fratello: #classe Fratello
     def __init__(self, nome, cognome, Salvadanaio):
         self.nome = nome
         self.cognome = cognome
-        self.salvadanaio = Salvadanaio()
+        self.salvadanaio = Salvadanaio #stessa istanza per ogni fratello
     def metti_soldi(self, somma):
         self.salvadanaio.aggiungi(somma)
     def controlla_risparmi(self):
         return self.salvadanaio.soldi
 
 def main():
-    soldi_iniziali = input("Inserisci la somma iniziale nel salvadanaio: ")
-    salvadanaio = Salvadanaio()
+    soldi_iniziali = int(input("Inserisci la somma iniziale nel salvadanaio: ")) 
+    salvadanaio = Salvadanaio(soldi_iniziali)
     fratello1 = Fratello("Mario", "Rossi", salvadanaio)
     fratello2 = Fratello("Luigi", "Verdi", salvadanaio)
     soldi_da_aggiungere1 = input("Inserisci la somma da aggiungere al salvadanaio (fratello1): ")
     fratello1.metti_soldi(int(soldi_da_aggiungere1))
     soldi_da_aggiungere2 = input("Inserisci la somma da aggiungere al salvadanaio (fratello2): ")
     fratello2.metti_soldi(int(soldi_da_aggiungere2))
+    print(fratello1.controlla_risparmi())
+
+main()

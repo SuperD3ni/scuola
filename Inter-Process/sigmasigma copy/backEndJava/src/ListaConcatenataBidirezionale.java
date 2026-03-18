f1c756ae86487e14c70db0612fea0bd081a0f74e
Denis <99391700+SuperD3ni@users.noreply.github.com>
2026-03-18 10:12:31 +0100

    Add Java classes for linked list implementation and GUI integration

---
+++ Inter-Process/sigmasigma copy/backEndJava/src/ListaConcatenataBidirezionale.java
@@ -0,0 +1,143 @@
+public class ListaConcatenataBidirezionale {
+    private NodeString head;
+    private NodeString tail;
+    private int size;
+
+    public ListaConcatenataBidirezionale() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public int size() { return size; }
+
+    public void inserimentoInTesta(String value) {
+        NodeString n = new NodeString(value);
+        if (head == null) {
+            head = tail = n;
+        } else {
+            n.setNext(head);
+            head.setPrev(n);
+            head = n;
+        }
+        size++;
+    }
+
+    public void inserimentoInCoda(String value) {
+        NodeString n = new NodeString(value);
+        if (tail == null) {
+            head = tail = n;
+        } else {
+            tail.setNext(n);
+            n.setPrev(tail);
+            tail = n;
+        }
+        size++;
+    }
+
+    public void inserimentoInMezzo(int index) {
+        NodeString n = new NodeString(value);
+        if (head != null && value.compareTo(head.getData()) > 0) {
+            NodeString p = head;
+            while (p.getNext() != null && value.compareTo(p.getNext().getData()) > 0) {
+                p = p.getNext();
+            }
+            n.setNext(p.getNext());
+            if (p.getNext() != null) p.getNext().setPrev(n);
+            p.setNext(n);
+            n.setPrev(p);
+            if (n.getNext() == null) tail = n;
+        } else {
+            n.setNext(head);
+            if (head != null) head.setPrev(n);
+            head = n;
+            if (tail == null) tail = n;
+        }
+        size++;
+    }
+
+    public void inserimentoIndietro(String value) {
+        NodeString n = new NodeString(value);
+        if (head == null) {
+            head = tail = n;
+            size++;
+            return;
+        }
+        NodeString p = tail;
+        while (p != null && p.getData().compareTo(value) >= 0) {
+            p = p.getPrev();
+        }
+        if (p == null) {
+            // inserisci in testa
+            n.setNext(head);
+            head.setPrev(n);
+            head = n;
+        } else {
+            // inserisci dopo p
+            n.setNext(p.getNext());
+            n.setPrev(p);
+            if (p.getNext() != null) p.getNext().setPrev(n);
+            p.setNext(n);
+            if (n.getNext() == null) tail = n;
+        }
+        size++;
+    }
+
+    public void visualizza() {
+        NodeString p = head;
+        while (p != null) {
+            System.out.print(p.getData());
+            p = p.getNext();
+        }
+        System.out.println();
+    }
+
+    public void visualizzaIndietro() {
+        NodeString p = tail;
+        while (p != null) {
+            System.out.print(p.getData());
+            p = p.getPrev();
+        }
+        System.out.println();
+    }
+    
+    public NodeString getHead() { return head; }
+    public void setHead(NodeString newHead) { head = newHead; }
+    
+    public NodeString getTail() { return tail; }
+    public void setTail(NodeString newTail) { tail = newTail; }
+    
+    public void stampaNodi() {
+        NodeString current = head;
+        while (current != null) {
+            System.out.println(current.getData());
+            current = current.getNext();
+        }
+    }
+    
+    public void rimuoviNodo(String nome) {
+        NodeString current = head;
+        while (current != null) {
+            if (current.getData().equals(nome)) {
+                NodeString prev = current.getPrev();
+                NodeString next = current.getNext();
+                
+                if (prev != null) {
+                    prev.setNext(next);
+                } else {
+                    head = next;
+                }
+                
+                if (next != null) {
+                    next.setPrev(prev);
+                } else {
+                    tail = prev;
+                }
+                
+                current = null;
+            } else {
+                current = current.getNext();
+            }
+        }
+    }
+    
---
+++ Inter-Process/verifica 21-01-26V2Lista copy/backend/src/Nodo.java
@@ -0,0 +1,30 @@
+public class Nodo <T>{
+    private T data;
+    private Nodo<T> next, prev;
+    public Nodo(T data) {
+        this.data = data;
+        this.next = null;
+    }
+    public Nodo(T data, Nodo<T> next) {
+        this.data = data;
+        this.next = next;
+    }
+    public T getData() {
+        return data;
+    }
+    public void setData(T data) {
+        this.data = data;
+    }
+    public Nodo<T> getNext() {
+        return next;
+    }
+    public void setNext(Nodo<T> next) {
+        this.next = next;
+    }
+    public Nodo<T> getPrev() {
+        return prev;
+    }
+    public void setPrev(Nodo<T> prev) {
+        this.prev = prev;
+    }
+}
